22d0611987b7a822e6c884f0d1a0e861ed58491f
rathish476 <72910651+rathish476@users.noreply.github.com>
2021-03-05 07:37:32 +0530

    Add files via upload

---
+++ speaker-app/src/main/java/com/xworkz/speaker/dao/SpeakerDAO.java
@@ -0,0 +1,7 @@
+package com.xworkz.speaker.dao;
+
+import com.xworkz.speaker.dto.SpeakerDTO;
+
+public interface SpeakerDAO {
+	public void save(SpeakerDTO dto);
+}
---
+++ speaker-app/src/main/java/com/xworkz/speaker/test/Tester.java
@@ -0,0 +1,16 @@
+package com.xworkz.speaker.test;
+
+import com.xworkz.speaker.dao.SpeakerDAO;
+import com.xworkz.speaker.dao.SpeakerDAOImpl;
+import com.xworkz.speaker.dto.SpeakerDTO;
+
+public class Tester {
+
+	public static void main(String[] args) {
+		
+     SpeakerDAO speakerDAO = new SpeakerDAOImpl();
+     SpeakerDTO speakerDTO = new SpeakerDTO(04,"realme",1500,"black","small",true);
+	speakerDAO.save(speakerDTO);
+	}
+
+}
