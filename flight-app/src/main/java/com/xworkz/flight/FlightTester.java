454300bd51059c1a73e59ad94e7b5000cd98f2c8
rathish476 <72910651+rathish476@users.noreply.github.com>
2021-03-08 13:27:16 +0530

    Update FlightTester.java

--- flight-app/src/main/java/com/xworkz/flight/FlightTester.java
+++ flight-app/src/main/java/com/xworkz/flight/FlightTester.java
@@ -13,7 +13,7 @@ public static void main(String[] args) {
 		FlightDTO flightDTO=new FlightDTO(3, "sun", "Bengaluru", "chikmagaluru", 80);
 		
 		//flightDAO.saveFlight(flightDTO);
-		//flightDAO.updateFlight(3);
+		//flightDAO.updateFlight(1);
 		flightDAO.readFlight(2);
 		  //flightDAO.deleteFlight(4);
 	}
