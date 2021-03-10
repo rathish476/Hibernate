fddb3e0263880285d5e3e72c63a74360973eff86
rathish476 <72910651+rathish476@users.noreply.github.com>
2021-03-10 22:57:24 +0530

    Update SpeakerServiceImpl.java

--- speaker-app/src/main/java/com/xworkz/speaker/service/SpeakerServiceImpl.java
+++ speaker-app/src/main/java/com/xworkz/speaker/service/SpeakerServiceImpl.java
@@ -8,86 +8,146 @@ public class SpeakerServiceImpl implements SpeakerService {
 
 	@Override
 	public boolean validateAndSave(SpeakerDTO dto) {
-		boolean validateDto = false;
+private SpeakerDAO dao;
 
-		if (dto != null) {
-			System.out.println("SpeakerDTO is not null,validate");
+	public SpeakerServiceImpl(SpeakerDAO SpeakerDAO) {
+		this.SpeakerDAO = dao;
+	}
 
-			int id = dto.getSpeakerID();
-
-			if (id > 0) {
-				System.out.println("Id is valid");
-				validateDto = true;
+	@Override
+	public boolean validateAndSaveSpeaker(SpeakerDTO dtO) {
+		boolean validData = false;
+		if (SpeakerDTO != null) {
+			System.out.println("dto is not null, will validate fields");
+			String brand = SpeakerDTO.getBrand();
+			if (brand != null && brand.length() >= 2 && !brand.isEmpty() && !brand.contains(" ")) {
+				System.out.println("brand is valid");
+				validData = true;
 			} else {
-				System.out.println("Id is invalid");
-				validateDto = false;
+				System.out.println("brand is invalid");
+				validData = false;
+			}
+		
+		if (validData) {
+			String SpeakerSeriesName = SpeakerDTO.getSpeakerSeriesName();
+			if (SpeakerSeriesName != null && SpeakerSeriesName.length() >= 4 && !SpeakerSeriesName.isEmpty()
+					&& !SpeakerSeriesName.contains(" ")) {
+				System.out.println("SpeakerSeriesName is valid");
+				validData = true;
+			} else {
+				System.out.println("SpeakerSeriesName is invalid");
+				validData = false;
 			}
 
-			// brand validation
-			if (validateDto) {
-				String brand = dto.getBrand();
-
-				if (brand != null && !brand.isEmpty() && !brand.contains(" ")) {
-					System.out.println("Brand is valid");
-					validateDto = true;
-				} else {
-					System.out.println("Brand is not valid");
-					validateDto = false;
-				}
-			}
-
-			// cost validation
-			if (validateDto) {
-				double cost = dto.getCost();
-
-				if (cost > 0) {
-					System.out.println("Cost is valid");
-					validateDto = true;
-				} else {
-					System.out.println("Cost is invalid");
-					validateDto = false;
-				}
-
-			}
-
-			// color validation
-
-			if (validateDto) {
-				String color = dto.getColor();
-
-				if (color != null && !color.isEmpty() && !color.contains(" ")) {
-					System.out.println("Color is valid");
-					validateDto = true;
-				} else {
-					System.out.println("Color is invalid");
-					validateDto = false;
-				}
-			}
-
-			// size validation
-
-			if (validateDto) {
-				String size = dto.getSize();
-
-				if (size != null && !size.isEmpty() && !size.contentEquals(" ")) {
-					System.out.println("Size is valid");
-					validateDto = true;
-				} else {
-					System.out.println("Size is invalid");
-					validateDto = false;
-				}
-			}
-
-			if (validateDto) {
-				System.out.println("DTO is valid");
-				SpeakerDAO speakerDAO = new SpeakerDAOImpl();
-				speakerDAO.save(dto);
-			}
-		} else {
-			System.out.println("DTO is invalid");
 		}
+		if (validData) {
+			double price = SpeakerDTO.getPrice();
+			if (price > 0) {
+				System.out.println("price is valid");
+				validData = true;
+			} else {
+				System.out.println("price is invalid");
+				validData = false;
+			}
 
+		}
+		if (validData) {
+			String color = SpeakerDTO.getColor();
+			if (color != null && color.length() >= 3 && !color.isEmpty() && !color.contains(" ")) {
+				System.out.println("color is valid");
+				validData = true;
+			} else {
+				System.out.println("color is invalid");
+				validData = false;
+			}
+		}
+		if (validData) {
+			double batteryCapacity = mobileDTO.getBatteryCapacity();
+			if (batteryCapacity > 0 && batteryCapacity <= 7000) {
+				System.out.println("batteryCapacity is valid");
+				validData = true;
+			} else {
+				System.out.println("batteryCapacity is invalid");
+				validData = false;
+			}
+
+		}
+		if (validData) {
+			System.out.println("data is valid , can invoke mobiledao");
+			SpeakerDAO.saveSpeaker(SpeakerDTO);
+		} else {
+			System.out.println("Speakerdto is null, invalid data");
+		}
+		}
 		return false;
 	}
 
+	@Override
+	public boolean validateAndRead(SpeakerDTO dto) {
+		int SpeakerId=0;
+		boolean validData = false;
+		if (SpeakerDTO != null) {
+			System.out.println("dto is not null, will validate fields");
+		    SpeakerId=SpeakerDTO.getSpeakerId();
+	        if(mSpeakerId > 0) {
+			    System.out.println("SpeakerId is valid");
+			    validData = true;
+		    } else {
+			    System.out.println("SpeakerId is invalid");
+			    validData = false;
+		}
+		}if (validData) {
+			System.out.println("data is valid , can invoke Speakerdao");
+			SpeakerDAO.readSpeaker(SpeakerId);
+		} else {
+			System.out.println("Speakerdto is null, invalid data");
+		}
+		return false;
+	}
+
+	@Override
+	public boolean validateAndUpdate(SpeakerDTO dto) {
+		int SpeakerId=0;
+		boolean validData = false;
+		if (SpeakerDTO != null) {
+			System.out.println("dto is not null, will validate fields");
+		    SpeakerId=SpeakerDTO.getSpeakerId();
+	        if(SpeakerId > 0) {
+			    System.out.println("SpeakerId is valid");
+			    validData = true;
+		    } else {
+			    System.out.println("SpeakerId is invalid");
+			    validData = false;
+		}
+		}if (validData) {
+			System.out.println("data is valid , can invoke mobiledao");
+			SpeakerDAO.updateSpeaker(SpeakerId);
+		} else {
+			System.out.println("Speakerdto is null, invalid data");
+		}
+		return false;
+	}
+
+	@Override
+	public boolean validateAndDelete(SpeakerDTO dto) {
+		int SpeakerId=0;
+		boolean validData = false;
+		if (SpeakerDTO != null) {
+			System.out.println("dto is not null, will validate fields");
+		   SpeakerId=SpeakerDTO.getSpeakerId();
+	        if(SpeakerId > 0) {
+			    System.out.println("SpeakerId is valid");
+			    validData = true;
+		    } else {
+			    System.out.println("SpeakerId is invalid");
+			    validData = false;
+		}
+		}if (validData) {
+			System.out.println("data is valid , can invoke mobiledao");
+			SpeakerDAO.deleteSpeaker(SpeakerId);
+		} else {
+			System.out.println("Speakerdto is null, invalid data");
+		}
+		return false;
+	}
 }
